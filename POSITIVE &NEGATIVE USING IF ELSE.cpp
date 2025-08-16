c4098ddda6dddad82647c8f1834eabfcba5d47fc
azhera375 <azhera375@gmail.com>
2025-08-16 12:32:10 +0530

    WAP POSITIVE  , NAGATIVE OR ZERO USING IF ELSE

---
+++ POSITIVE &NEGATIVE USING IF ELSE.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+main() {
+int num;
+printf("enter the number");
+scanf("%d",&num);
+if (num>=0)
+{
+if(num>0)
+{
+printf("NUMBER IS POSITIVE");
+}
+else
+{
+    printf("NUMBER IS ZERO");
+}}
+
+else
+{
+    printf("NUMBER IS NEGATIVE");
+}
+}
